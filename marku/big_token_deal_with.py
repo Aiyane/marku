ffce3ab31a7080143d0e885a0f473f3ac2b0fb7f
qq2310091880 <2310091880@qq.com>
2018-01-10 02:10:05 +0800

    精简代码

--- marku/big_token_deal_with.py
+++ marku/big_token_deal_with.py
@@ -17,14 +17,10 @@ def deal_with(lines, token_types, init_token, root=None):
     line_buffer = []
     for line in lines:
         line = line.replace('\t', ' ' * 4)
-        if line.startswith('```'):
+        if is_code or line != '\n':
             line_buffer.append(line)
-            if is_code:
-                is_code = False
-            else:
-                is_code = True
-        elif is_code or line != '\n':
-            line_buffer.append(line)
+            if line.startswith('```'):
+                is_code = not is_code
         elif line == '\n' and line_buffer:
             yield find_token(line_buffer, token_types, init_token)
             line_buffer.clear()
