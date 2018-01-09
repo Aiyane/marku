1a8ffce0c74a00f1d6cce14852c22b469b7ba94f
qq2310091880 <2310091880@qq.com>
2018-01-10 01:41:04 +0800

    重写了块token处理函数

--- marku/big_token_deal_with.py
+++ marku/big_token_deal_with.py
@@ -3,41 +3,6 @@
 __author__ = "Aiyane"
 
 
-class codeLine(str):
-    """这是一个持续的行, 虽然只有换行符, 但是还是要保留它, 因为它在code里"""
-
-    def __ne__(self, other):
-        if other == '\n':
-            return True
-
-
-def init_deal(lines):
-    """这是一个初始化处理lines的方法
-    在```something```里面可以有空行, 所以要区分出来
-    :lines: 多行
-    :returns: 特殊的一行
-
-    """
-    code_fence = False
-    for line in lines:
-        # 格式化tabs
-        line = line.replace('\t', '    ')
-
-        if line.startswith('```'):
-            if code_fence:
-                yield line
-                yield '\n'
-                code_fence = False
-            else:
-                code_fence = True
-                yield '\n'
-                yield line
-        elif code_fence:
-            yield codeLine(line)
-        else:
-            yield line
-
-
 def deal_with(lines, token_types, init_token, root=None):
     """这是一个Token处理函数
 
@@ -48,17 +13,25 @@ def deal_with(lines, token_types, init_token, root=None):
     :returns: 返回Token
 
     """
+    is_code = False
     line_buffer = []
-    for line in init_deal(lines):
-        # 这一段就是要区分块级的Token
-        if line != '\n':
+    for line in lines:
+        line = line.replace('\t', ' ' * 4)
+        if line.startswith('```'):
             line_buffer.append(line)
-        elif line_buffer:
-            for token_type in token_types:
-                if token_type.match(line_buffer):
-                    yield token_type(line_buffer)
-                    line_buffer.clear()
-                    break
-            if line_buffer:
-                yield init_token(line_buffer)
-                line_buffer.clear()
+            if is_code:
+                is_code = False
+            else:
+                is_code = True
+        elif is_code or line != '\n':
+            line_buffer.append(line)
+        elif line == '\n' and line_buffer:
+            yield find_token(line_buffer, token_types, init_token)
+            line_buffer.clear()
+
+
+def find_token(line_buffer, token_types, init_token):
+    for token_type in token_types:
+        if token_type.match(line_buffer):
+            return token_type(line_buffer)
+        return init_token(line_buffer)
