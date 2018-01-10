2347c0007430867cbd03988c385055b7c9a25f1f
qq2310091880 <2310091880@qq.com>
2018-01-10 19:17:43 +0800

    增加测试相关

---
+++ test/test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Aiyane'
+
+import sys
+import os
+
+from marku.big_token.py import DocumentToken
+from marku.HTML_render.py import HTMLRenderer
+
+argv = sys.argv
+
+try:
+    with open(argv[1], 'r', encoding='utf-8') as fin:
+        AST = DocumentToken(fin)
+        rendered = HTMLRenderer().render(AST)
+    with open(argv[2], 'w') as f:
+        f.write(rendered)
+except Exception:
+    with open(os.path.split(os.path.realpath(__file__))[0] + '\\test.md', 'r', encoding='utf-8') as fin:
+        AST = DocumentToken(fin)
+        rendered = HTMLRenderer().render(AST)
+    with open('output.html', 'w') as f:
+        f.write(rendered)
