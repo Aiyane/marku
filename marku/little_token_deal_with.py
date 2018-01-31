ce67e93480df57a04acb3e5298ebd9c3f35e004c
aiyane <2310091880@qq.com>
2018-02-01 00:38:39 +0800

    增加注释

--- marku/little_token_deal_with.py
+++ marku/little_token_deal_with.py
@@ -7,8 +7,9 @@ def deal_with_line(content, token_types, init_token):
     """处理行内Token, 最开始写的代码太丑了, 最后精简到20行以内, 同时修复了之前的Bug
     参数:
         :index: 这是一个列表, 0号位表示起始位置, 1号位表示匹配的末尾位置
-        :match_obj: 这是临时匹配到的token
         :minIndex: 这是匹配的起始位置, 我们需要找出近匹配的位置
+        :init_token: 这是默认的匹配Token， 用于匹配没有找到的Token类型， 一般默认为段落
+        :close_token: 这是生成的Token的buffer
     """
     index = [0, 0]
     while 1:
@@ -20,9 +21,11 @@ def deal_with_line(content, token_types, init_token):
                 minIndex = match_obj.start()
                 close_token = token_type(match_obj)
                 index[1] = match_obj.end()
+        # 如果起始位置并不是第一个匹配的位置就在前段生成默认类型
         yield init_token(
             content[index[0]:minIndex]) if index[0] < minIndex else None
         if not close_token:
             break
+        # 如果匹配到了就让起始位置改变， 再考察后面的字符
         index[0] = index[1]
         yield close_token
